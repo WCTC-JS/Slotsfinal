ef0a6fd67cb3e4b231d1557706d681dc350b2c30
Luis Diego Hernandez <diegotapia1987@gmail.com>
2020-12-09 10:10:30 -0600

    diego02

---
+++ validation.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+    var myRules = {
+        name: {
+            required: true,
+            min: 2
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        phoneNumber: {
+            min: 10,
+            digits: true
+        },
+        message: {
+            required: true,
+            min: 5,
+            max: 200
+        }
+    };
+
+    var myMessages = {
+        name: {
+            required: "Fill this in!",
+            min: "Enter a valid name"
+        },
+        email: {
+            required: "Fill this in!",
+            email: "Enter a valid E-mail"
+        },
+        phoneNumber: {
+            min: "Enter a valid phone number",
+            digits: "Enter a valid phone number"
+        },
+        message: {
+            required: "Fill this in!",
+            min: "You message is too short!",
+            max: "You message is too long!"
+        }
+    };
+
+
+    $("form").validate({
+        submitHandler: contactUs,
+        rules: myRules,
+        messages: myMessages
+    });
+
+    function contactUs()
+    {
+        var customerName = $("#name").val();
+        var customerEmail = $("#email").val();
+        var customerPhoneNumber = $("#email").val();
+        var customerMessage = $("#message").val();
+
+        $("p").text(`You fed ${customerName} bananas to ${customerEmail} koalas ${customerPhoneNumber} ${customerMessage}`);
+    }
+
+
+});
